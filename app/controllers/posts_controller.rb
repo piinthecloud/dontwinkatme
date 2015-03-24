cf7619263b57ad7a61f4671fd853061673ae4455
Stephanie <stephmpi@gmail.com>
2015-03-24 10:08:16 -0700

    initial commit for heathers project

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+end
